7396d59ff97a5101fedc6fa6c7ef499caf942e01
Sergey <santerakis@gmail.com>
2022-12-17 11:41:22 +0200

    HW6_start

---
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -0,0 +1,94 @@
+import React, {useState} from 'react'
+import Affairs from './affairs/Affairs'
+import s2 from '../../s1-main/App.module.css'
+
+/*
+* 1 - описать типы AffairPriorityType, AffairType
+* 2 - указать нужный тип для defaultAffairs
+* 3 - дописать типы и логику функции filterAffairs и проверить её тестами
+* 4 - выполнить пункт 3 для функции deleteAffair
+* 5 - указать нужный тип в useState с affairs
+* 6 - дописать тип и логику функции deleteAffairCallback
+* 7 - в файле Affairs.tsx дописать типизацию пропсов
+* 8 - в файле Affairs.tsx дописать логику функций setAll, setHigh, setMiddle, setLow
+* 9 - в файле Affair.tsx дописать типизацию пропсов
+* 10 - в файле Affair.tsx дописать функции deleteCallback и использовать
+* 11 - в файле Affair.tsx отобразить приходящие данные
+* */
+
+// types
+export type FilterType = 'all' | AffairPriorityType
+
+export type AffairPriorityType = 'high' | 'middle' | 'low' // need to fix any
+export type AffairType = {
+    _id: number // need to fix any
+    name: string // need to fix any
+    priority: AffairPriorityType
+}
+// constants
+const defaultAffairs: AffairType[] = [ // need to fix any
+    {_id: 1, name: 'React', priority: 'high'}, // студенты могут изменить содержимое name и количество элементов в массиве, ...priority не менять!
+    {_id: 2, name: 'anime', priority: 'low'},
+    {_id: 3, name: 'games', priority: 'low'},
+    {_id: 4, name: 'work', priority: 'high'},
+    {_id: 5, name: 'html & css & js', priority: 'middle'},
+]
+
+// pure helper functions
+export const filterAffairs = (affairs: Array<AffairType>, filter: FilterType): Array<AffairType> => { // need to fix any
+    //let filAffairs = affairs
+    if (filter === 'high') {
+        //filAffairs =
+        return affairs.filter(affair => affair.priority === 'high')
+    }
+    if (filter === 'middle') {
+        // filAffairs =
+        return affairs.filter(affair => affair.priority === 'middle')
+    }
+    if (filter === 'low') {
+        //filAffairs =
+        return affairs.filter(affair => affair.priority === 'low')
+    }
+
+    return affairs//affairs // need to fix
+}
+export const deleteAffair = (affairs: Array<AffairType>, _id: number): Array<AffairType> => { // need to fix any
+
+    return affairs.filter(affair => affair._id !== _id)//affairs // need to fix
+}
+
+// Array<AffairType>
+// AffairType[]
+
+//Array<number>
+//number[]
+
+// useState<number>()
+
+function HW2() {
+    const [affairs, setAffairs] = useState<AffairType[]>(defaultAffairs) // need to fix any
+    const [filter, setFilter] = useState<FilterType>('all')
+
+    const filteredAffairs = filterAffairs(affairs, filter)
+
+    const deleteAffairCallback = (_id: number) => { // need to fix any
+        deleteAffair(affairs, _id)
+        setAffairs(deleteAffair(affairs, _id))// need to fix
+    }
+
+    return (
+        <div id={'hw2'}>
+            <div className={s2.hwTitle}>Homework_#2</div>
+            <div className={s2.hw}>
+                <Affairs
+                    data={filteredAffairs}
+                    setFilter={setFilter}
+                    deleteAffairCallback={deleteAffairCallback}
+                    filter={filter}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default HW2
